0b56c3bc411b4cbdbe12534d39295a53afe1128b
tamara96mc <tamara.96mc@gmail.com>
2020-12-08 21:05:56 +0100

    Updates

---
+++ precache-manifest.17d6016acb8b0fb19c09c4da3f79e1c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4806f9bde3e95ef0e00769feb852c7cf",
+    "url": "/clima/index.html"
+  },
+  {
+    "revision": "010d88c9e74aca91b80f",
+    "url": "/clima/static/css/main.11aa377c.chunk.css"
+  },
+  {
+    "revision": "23bc317c3c224d1c1748",
+    "url": "/clima/static/js/2.58f2b4fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clima/static/js/2.58f2b4fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "010d88c9e74aca91b80f",
+    "url": "/clima/static/js/main.7310ae17.chunk.js"
+  },
+  {
+    "revision": "df1c1c8ea8193d3bfd76",
+    "url": "/clima/static/js/runtime-main.f845c372.js"
+  }
+]);
